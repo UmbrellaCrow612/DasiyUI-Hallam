8c6223b48037f7a401ee8d800984dde8c0eb0bb6
yousaf wazir <umbrellaman612@gmail.com>
2022-08-31 18:27:58 +0100

    feat: added link highlighting

--- src/components/MobileMenuHandler.tsx
+++ src/components/MobileMenuHandler.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { BiMenuAltLeft } from 'react-icons/bi'
 
 interface MobileMenuHandlerProps {}
 
 export const MobileMenuHandler: React.FC<MobileMenuHandlerProps> = () => {
+  const router = useRouter()
+  const [url, setUrl] = useState('')
+  useEffect(() => {
+    setUrl(router.pathname)
+  }, [router.pathname])
   return (
     <>
+      {/* For desktop  - dropdown  */}
       <div className="hidden dropdown md:block">
         <label tabIndex={0} className="m-1 btn">
           <BiMenuAltLeft className="text-2xl" />
@@ -15,16 +23,26 @@ export const MobileMenuHandler: React.FC<MobileMenuHandlerProps> = () => {
           className="p-2 shadow dropdown-content menu bg-base-100 rounded-box w-52"
         >
           {Options.map((Option) => (
-            <li key={Option}>
-              <Link href={`/${Option.toLowerCase()}`}>
-                <a>{Option.replace('-', ' ')}</a>
+            <li
+              key={Option}
+              className={url === `/${Option.toLowerCase()}` && "bordered" || ""}
+            >
+              <Link href={`${Option.toLowerCase()}`}>
+                <a>
+                  {Option.replace('-', ' ')}
+                </a>
               </Link>
             </li>
           ))}
         </ul>
       </div>
 
-      <label htmlFor="my-drawer" className="btn drawer-button md:hidden" aria-label='Open menu'>
+      {/* For mobile - Drawer  */}
+      <label
+        htmlFor="my-drawer"
+        className="btn drawer-button md:hidden"
+        aria-label="Open menu"
+      >
         <BiMenuAltLeft className="text-2xl" />
       </label>
     </>
