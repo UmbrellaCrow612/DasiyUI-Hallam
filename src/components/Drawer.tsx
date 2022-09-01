427ac3cc3caaf964ccf2a9726916cc356fe4531c
yousaf wazir <umbrellaman612@gmail.com>
2022-09-01 13:46:58 +0100

    feat: added error handler

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,12 +1,25 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 interface DrawerProps {
   children: React.ReactNode
 }
 
 export const Drawer: React.FC<DrawerProps> = ({ children }) => {
+    const router = useRouter()
+    const [url, setUrl] = useState('')
+    useEffect(() => {
+      setUrl(router.pathname)
+    }, [router.pathname])
   return (
     <>
       <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" placeholder="Drawer"/>
+        <input
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+          placeholder="Drawer"
+        />
         <div className="drawer-content">
           {/* <!-- Page content here --> */}
           {children}
@@ -16,15 +29,34 @@ export const Drawer: React.FC<DrawerProps> = ({ children }) => {
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
           <ul className="p-4 overflow-y-auto menu w-80 bg-base-100 text-base-content">
             {/* <!-- Sidebar content here --> */}
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+            {sideBarOptions.map((Option) => (
+              <li
+                key={Option.toLowerCase()}
+                className={
+                  (url === `/${Option.toLowerCase()}` && 'bordered') || ''
+                }
+              >
+                <Link href={`/${Option.toLowerCase()}`}>
+                  <a>{Option.replace('-', ' ')}</a>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </>
   )
 }
+
+const sideBarOptions = [
+  'Student',
+  'Applicant',
+  'International',
+  'Study here',
+  'International',
+  'Business',
+  'Research',
+  'Alumni',
+  'Visit-us',
+  'About-us',
+]
--- src/components/LogoButton.tsx
+++ src/components/LogoButton.tsx
@@ -1,15 +1,21 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { AiOutlineHome } from 'react-icons/ai'
 
 interface LogoButtonProps {}
 
 export const LogoButton: React.FC<LogoButtonProps> = () => {
   const router = useRouter()
+  const [url, setUrl] = useState('')
+  useEffect(() => {
+    setUrl(router.pathname)
+  }, [router.pathname])
+  console.log(url)
   return (
     <>
       <button
         aria-label="Home"
-        className="btn"
+        className={url === '/_error' ? 'btn btn-primary' : 'btn'}
         onClick={() => router.push('/')}
       >
         <AiOutlineHome className="text-2xl" />
